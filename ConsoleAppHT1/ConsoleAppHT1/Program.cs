cdabf73abc3b2c4b756da76280870a578443d6cc
Alma Didic <AMDD@simcorp.com>
2023-05-23 15:31:08 +0200

    Revert "Development 2 in feature branch 1"
    This reverts commit 8a811da4804498be98d5d5bbe99f8ff63f231b05.

--- ConsoleAppHT1/ConsoleAppHT1/Program.cs
+++ ConsoleAppHT1/ConsoleAppHT1/Program.cs
@@ -11,7 +11,6 @@ namespace ConsoleAppHT1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
-            Console.WriteLine("Feature branch 1 - development 2");
         }
     }
 }
