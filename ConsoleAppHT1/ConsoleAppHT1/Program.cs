502f9be9cbfe82c7837c8ac46516a2944dcc0cf1
Alma Didic <AMDD@simcorp.com>
2023-05-24 16:01:14 +0200

    Calculation of current value based on user input

--- ConsoleAppHT1/ConsoleAppHT1/Program.cs
+++ ConsoleAppHT1/ConsoleAppHT1/Program.cs
@@ -6,13 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppHT1
 {
+    enum TransactionType
+    {
+        Buy=1, Sell=-1
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
-            Console.WriteLine("Feature branch 1 - development 1");
-            Console.WriteLine("Feature branch 1 - development 2");
+            int nominal;
+            float price, curVal;
+            TransactionType transType;
+
+            Console.WriteLine("Please enter the nominal of the transaction:");
+            nominal = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter the trading price (decimals separated by comma):");
+            price = float.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Please enter the type of transaction: {TransactionType.Buy} or {TransactionType.Sell} ");
+            transType= (TransactionType)Enum.Parse(typeof(TransactionType), Console.ReadLine(), true);
+
+            curVal = nominal * price * ((int)transType);
+        
+            Console.WriteLine("The calculated current value of your transaction is {0}", curVal);
+            Console.ReadLine();
         }
     }
 }
