febebceab8d1aa4c9d608dcc9ca34147bef1a621
Alma Didic <AMDD@simcorp.com>
2023-05-24 16:41:19 +0200

    Small corrections

--- ConsoleAppHT1/ConsoleAppHT1/Program.cs
+++ ConsoleAppHT1/ConsoleAppHT1/Program.cs
@@ -15,10 +15,10 @@ namespace ConsoleAppHT1
         static void Main(string[] args)
         {
             int nominal;
-            double priceTrade, priceOrg; 
-            double profitLoss, currentValue; 
+            double priceTrade, priceOrg, profitLoss, currentValue; 
             TransactionType transType;
 
+            //Gather input
             Console.WriteLine("Please enter the nominal of the transaction:");
             nominal = int.Parse(Console.ReadLine());
 
@@ -31,9 +31,11 @@ namespace ConsoleAppHT1
             Console.WriteLine($"Please enter the type of transaction: {TransactionType.Buy} or {TransactionType.Sell} ");
             transType = (TransactionType)Enum.Parse(typeof(TransactionType), Console.ReadLine(), true);
 
+            //Do the calculations
             currentValue = nominal * priceTrade * ((int)transType);
             profitLoss = transType.Equals(TransactionType.Sell) ? (priceTrade - priceOrg)*nominal : 0;
 
+            //Display results
             Console.WriteLine("The calculated current value of your transaction is {0}", currentValue);
             Console.WriteLine($"The calculated profit loss of your {transType} transaction is {profitLoss}");
             Console.ReadLine();
